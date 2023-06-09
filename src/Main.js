81e4f5a47e7fbf03b38bbbf44cbbc68d18954666
leandro-lisa <leandro.lisa@uala.com.ar>
2023-06-09 09:16:16 -0300

    adding Menu

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import { MenuAppBar } from './pages/appbar/MenuAppbar';
+import './Main.css';
+
+function App() {
+  return (
+    <div>
+        <MenuAppBar />
+    </div>
+  );
+}
+
+export default App;
