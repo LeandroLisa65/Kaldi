7fd5ced149c76715a5af2d455d834cec97270e86
leandro-lisa <leandro.lisa@uala.com.ar>
2023-07-03 22:42:05 -0300

    dynamic categories

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import ItemDetailContainer from "../ItemDetailContainer/ItemDetailContainer";
 import Loading from "../Loading/Loading";
 import { Link } from "react-router-dom";
-import { getItems } from "../../asyncMock";
+import { getCategories, getItems } from "../../asyncMock";
 
 import "./ItemList.scss";
 
@@ -10,7 +10,7 @@ import "./ItemList.scss";
 const ItemList = ({ onAdd }) => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
-
+  const [categories, setCategories] = useState([]);
   useEffect(() => {
     setLoading(true);
       getItems()
@@ -19,6 +19,19 @@ const ItemList = ({ onAdd }) => {
           console.log("No results!");
         }
         setProducts(items);
+      })
+      .catch((error) => {
+        console.log("Error searching items", error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+      getCategories()
+      .then((items) => {
+        if (items.length === 0) {
+          console.log("No results!");
+        }
+        setCategories(items);
       })
       .catch((error) => {
         console.log("Error searching items", error);
@@ -37,9 +50,10 @@ const ItemList = ({ onAdd }) => {
         <div className="list-container__details">
           <div className="list-container__details-categories">
             <h2>Categorías</h2>
-            <Link to="/categories/1">Categoría 1</Link>
-            <Link to="/categories/2">Categoría 2</Link>
-            <Link to="/categories/3">Categoría 3</Link>
+            {categories.map((category) => {
+              let id = category.id;
+              return <Link to={'/categories/'+id}>{category.description}</Link>;
+            })}
           </div>
           <div className="list-container__details-title">
             <h3>Productos</h3>
